817f910530d24a37e20f89c325c7b23c4dd4a39d
AlexHuk85 <alex.huk85@gmail.com>
2020-07-08 09:30:25 +1000

    adjust table

--- backend.py
+++ backend.py
@@ -3,14 +3,14 @@ import sqlite3
 def connect():
     conn = sqlite3.connect("partlist.db")
     cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS parts (id INTEGER PRIMARY KEY,part_name text, part_number text, Qty integer,model text)")
+    cur.execute("CREATE TABLE IF NOT EXISTS parts (id INTEGER PRIMARY KEY, model TEXT, part_name TEXT, part_number TEXT, Qty INTEGER)")
     conn.commit()
     conn.close()
 
-def add(part_name,part_number,Qty,model):
+def add(model,part_name,part_number,Qty):
     conn = sqlite3.connect("partlist.db")
     cur = conn.cursor()
-    cur.execute("INSERT INTO parts VALUES (NULL,?,?,?,?)",(part_name,part_number,Qty,model))
+    cur.execute("INSERT INTO parts VALUES (NULL,?,?,?,?)",(model,part_name,part_number,Qty))
     conn.commit()
     conn.close()
 
@@ -25,5 +25,5 @@ def view():
 
 
 connect()
-#add('testing','C808xx-xx-xxxx',5,'OS808-uDivine')
+add('OS808-uDivine','testing','C808xx-xx-xxxx',5)
 print(view())
