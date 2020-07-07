e2ad743a96a8a50c9cf982fe0c1f2714bc7f1d16
AlexHuk85 <alex.huk85@gmail.com>
2020-07-08 09:02:12 +1000

    add db

--- backend.py
+++ backend.py
@@ -6,3 +6,15 @@ def connect():
     cur.execute("CREATE TABLE IF NOT EXISTS parts (id INTEGER PRIMARY KEY,part_name text, part_number text, Qty integer,model text)")
     conn.commit()
     conn.close()
+
+def add(part_name,part_number,Qty,model):
+    conn = sqlite3.connect("partlist.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO parts WHERE part_name=?,part_number=?,Qty=?,model=?",(part_name,part_number,Qty,model))
+    conn.commit()
+    conn.close()
+
+
+
+connect()
+add('testing','C808xx-xx-xxxx',5,'OS808-uDivine')
